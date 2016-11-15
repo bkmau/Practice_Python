c49ec07ae92d6670f6c9f721ac435e884ffbd4c1
bkmau <ft52000f@gmail.com>
2016-11-16 00:32:38 +0800

    Create BestTimeWithOneChoice.py

---
+++ BestTime/BestTimeWithOneChoice.py
@@ -0,0 +1,20 @@
+list = [3, 2, 6, 5, 6, 1, 2, 6, 1]
+list2 = [x - max(list) for x in list]
+profit = 0
+day_to_buy = 0
+day_to_sell = 0
+
+for i, j in enumerate(list2):
+    if j == 0:
+        if i > day_to_sell:
+            day_to_sell = i
+if day_to_sell != 0:
+    profit = abs(min(list2[0:day_to_sell]))
+    day_to_buy = list2.index(min(list2[0:day_to_sell])) + 1
+
+if profit == 0:
+    print("Can't get any profit in this investment")
+else:
+    print("To earn the best profit, {}, of stock, as buy at day {} and sell at day {}".format(
+        profit, day_to_buy, day_to_sell
+    ))
